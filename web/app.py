787b21062efd3a95c427f670a0c1206e00b48951
ukena <48249968+ukena@users.noreply.github.com>
2022-07-21 14:59:26 +0200

    changed metar.py

--- web/app.py
+++ web/app.py
@@ -9,18 +9,29 @@ def index():
         config = yaml.safe_load(f)
 
     if request.method == "POST":
+        print(request.form)
         for key, data in request.form.items():
             if key in ("ssid", "passwort"):
                 config["wlan"][key] = data
-            elif key in ("vfr", "vfr_bei_wind", "mvfr", "mvfr_bei_wind", "ifr", "ifr_bei_wind", "lifr", "lifr_bei_wind", "blitze", "sturm"):
+            elif key in ("vfr", "vfr_bei_wind", "mvfr", "mvfr_bei_wind", "ifr", "ifr_bei_wind", "lifr", "lifr_bei_wind", "blitze", "hoher_wind"):
                 config["farben"][key] = data
             elif key in ("normal", "hoch", "frequenz"):
                 config["wind"][key] = data
             elif key in ("an", "aus"):
                 config["zeiten"][key] = data
+            elif key == "update-branch":
+                if data == "main":
+                    # subprocess.call(["git", "checkout", "main"])
+                    # subprocess.call(["git", "pull", "origin", "main"])
+                    pass
+                elif data == "dev":
+                    # subprocess.call(["git", "checkout", "dev"])
+                    # subprocess.call(["git", "pull", "origin", "dev"])
+                    pass
         with open("config.yaml", "w") as f:
             yaml.dump(config, f)
-        # TODO: stop AP, start WLAN, connect
+        # TODO: write wifi setting into wpa_supplicant.conf, reboot
+
 
     return render_template("index.html", config=config)
 
