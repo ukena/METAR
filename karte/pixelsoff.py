7c6970b6fca087d2573b22e02a1d247310956ad4
ukena <48249968+ukena@users.noreply.github.com>
2022-07-30 15:48:09 +0200

    removed displays

--- karte/pixelsoff.py
+++ karte/pixelsoff.py
@@ -1,16 +1,8 @@
 import board
 import neopixel
-try:
-	import displaymetar
-except ImportError:
-	displaymetar = None
 
-pixels = neopixel.NeoPixel(board.D18, 50)
+pixels = neopixel.NeoPixel(board.D18, 100)
 
 pixels.deinit()
-
-if displaymetar is not None:
-	disp = displaymetar.startDisplay()
-	displaymetar.shutdownDisplay(disp)
 
 print("LEDs off")
