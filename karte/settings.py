2a08d403f560c82094726922fcddc6c201a4e0bc
ukena <48249968+ukena@users.noreply.github.com>
2022-08-08 19:30:40 +0200

    updated crontab

---
+++ karte/settings.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import subprocess
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def load_settings(channel):
+    with open("/etc/wpa_supplicant/wpa_supplicant-wlan0.conf", "r") as f:
+        in_file = f.readlines()
+        f.close()
+
+    out_file = []
+    edited_ssid = False
+    for line in in_file:
+        if "ssid" in line and not edited_ssid:
+            line = '    ssid=' + '"123456789101112"' + '\n'
+            edited_ssid = True
+        out_file.append(line)
+
+    with open("/etc/wpa_supplicant/wpa_supplicant-wlan0.conf", "w") as f:
+        for line in out_file:
+            f.write(line)
+
+    subprocess.call(["reboot"])
+
+GPIO.add_event_detect(23, GPIO.FALLING, callback=load_settings, bouncetime=2000)
+
+while 1:
+    time.sleep(1)
