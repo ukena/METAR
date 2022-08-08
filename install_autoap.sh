42b46c0e6c43ee3f877917e3e6bcf5b17ab0024c
torbenukena <torben.ukena@diva-e.com>
2022-08-08 10:32:47 +0200

    included airport list in config.yaml

--- install_autoap.sh
+++ install_autoap.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+# install autoap
 sudo curl -L https://github.com/ukena/autoAP/raw/master/autoAP.sh -o /usr/local/bin/autoAP.sh
 sudo curl -L https://github.com/ukena/autoAP/raw/master/install-autoAP -o /usr/local/bin/install-autoAP
 sudo curl -L https://github.com/ukena/autoAP/raw/master/rpi-networkconfig -o /usr/local/bin/rpi-networkconfig
 sudo chmod 755 /usr/local/bin/autoAP.sh /usr/local/bin/install-autoAP /usr/local/bin/rpi-networkconfig
 printf "METAR\nMETARKARTE\n\ny\n" | sudo /usr/local/bin/install-autoAP
 printf "\n" | sudo /usr/local/bin/rpi-networkconfig
+# handle permissions
+sudo chmod +x /home/pi/karte/refresh.sh
+sudo chmod +x /home/pi/karte/lightsoff.sh
+sudo chmod +r /home/pi/karte/metar.py
+sudo chmod +r /home/pi/karte/pixelsoff.py
+sudo chmod +r /home/pi/config.yaml
+# reboot
 sudo reboot
