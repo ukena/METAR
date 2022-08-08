68304c43a0f84473e745967ed687bc91ba0d9259
ukena <48249968+ukena@users.noreply.github.com>
2022-08-08 12:36:12 +0200

    updated crontab

--- karte/refresh.sh
+++ karte/refresh.sh
@@ -1,3 +1,3 @@
-/usr/bin/sudo pkill -F /home/pi/karte/offpid.pid
-/usr/bin/sudo pkill -F /home/pi/karte/metarpid.pid
-/usr/bin/sudo /usr/bin/python3 /home/pi/karte/metar.py & echo $! > /home/pi/karte/metarpid.pid
+pkill -F /home/pi/karte/offpid.pid
+pkill -F /home/pi/karte/metarpid.pid
+/usr/bin/python3 /home/pi/karte/metar.py & echo $! > /home/pi/karte/metarpid.pid
