06fa815aeb9808e9df32608ceb654b2d345992b3
ukena <48249968+ukena@users.noreply.github.com>
2022-08-08 11:32:26 +0200

    updated crontab

--- karte/metar.py
+++ karte/metar.py
@@ -46,7 +46,7 @@ print("Running metar.py at " + datetime.datetime.now().strftime('%d/%m/%Y %H:%M'
 pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, pixel_order=LED_ORDER, auto_write=False)
 
 # Read the airports file to retrieve list of airports and use as order for LEDs
-airports = [x.strip() for x in config.flugplaetze]
+airports = [x.strip() for x in config["flugplaetze"]]
 
 url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=5&mostRecentForEachStation=true&stationString=" + ",".join(
     [item for item in airports if item != "NULL"])
