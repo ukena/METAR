a8e02ba173244e5b017c6a0942719d2927e3fe08
ukena <48249968+ukena@users.noreply.github.com>
2022-08-08 21:17:12 +0200

    updated crontab

--- install.sh
+++ install.sh
@@ -14,6 +14,6 @@ sudo chmod +r /home/pi/karte/metar.py
 sudo chmod +r /home/pi/karte/pixelsoff.py
 sudo chmod +r /home/pi/config.yaml
 # include karte/settings.py in rc.local
-sudo sed -i '18i  sudo /usr/bin/python /home/pi/karte/settings.py > /home/pi/karte/settings-log.txt 2>&1 &' /etc/rc.local
+sudo sed -i '20isudo /usr/bin/python /home/pi/karte/settings.py > /home/pi/karte/settings-log.txt 2>&1 &' /etc/rc.local
 # autoAP installieren
 sudo /home/pi/install_autoap.sh
