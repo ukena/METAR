6f296bd74a7018b0055601fd5dbf40912b7e86a0
ukena <48249968+ukena@users.noreply.github.com>
2022-08-23 08:31:12 +0200

    fixed wrong pip packages

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 # pip installieren
 sudo apt install python3-pip -y
 # pip requirements installieren
-pip install -r /home/pi/requirements.txt
+suod pip3 install -r /home/pi/requirements.txt
+sudo python3 -m pip install --force-reinstall adafruit-blinka
 # berechtigungen vergeben
 sudo chmod +x /home/pi/install_autoap.sh
 sudo chmod +x /home/pi/karte/refresh.sh
@@ -13,4 +14,5 @@ sudo chmod +r /home/pi/config.yaml
 # include karte/settings.py in rc.local
 sudo sed -i '20isudo /usr/bin/python /home/pi/karte/settings.py > /home/pi/karte/settings-log.txt 2>&1 &' /etc/rc.local
 # autoAP installieren
+# TODO: ssh bricht ab, daher wird script nicht beendet
 sudo /home/pi/install_autoap.sh
