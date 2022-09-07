1f3289a41f44e9d1e14757a6ca5f633a25d09ab0
ukena <48249968+ukena@users.noreply.github.com>
2022-09-07 10:29:57 +0200

    fixed updates

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ suod pip3 install -r /home/pi/requirements.txt
 sudo python3 -m pip install --force-reinstall adafruit-blinka
 # berechtigungen vergeben
 sudo chmod +x /home/pi/handle_permissions.sh
+sudo /home/pi/handle_permissions.sh
 # include karte/settings.py in rc.local
 sudo sed -i '20isudo /usr/bin/python /home/pi/karte/settings.py > /home/pi/karte/settings-log.txt 2>&1 &' /etc/rc.local
 # autoAP installieren
