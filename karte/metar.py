a242dc63671b1914e7f53c62112b1d6787910e46
ukena <48249968+ukena@users.noreply.github.com>
2022-10-05 18:34:26 +0200

    changed network settings to use wpa_cli

--- karte/metar.py
+++ karte/metar.py
@@ -51,94 +51,127 @@ COLOR_GAFOR_X = hex_to_grb(config["farben_gafor"]["xray"])
 COLOR_GAFOR_X_WIND = hex_to_grb(config["farben_gafor"]["xray_bei_wind"])
 COLOR_GAFOR_BLITZE = hex_to_grb(config["farben_gafor"]["blitze-gafor"])
 
+# Windgeschwindigkeiten und Frequenz
 WIND_BLINK_THRESHOLD = int(config["wind"]["normal"])
 HIGH_WINDS_THRESHOLD = int(config["wind"]["hoch"])
 BLINK_SPEED = float(config["wind"]["frequenz"])
-
 BLINK_TOTALTIME_SECONDS = 300
 
-# Initialize the LED strip
+# LEDs instanziieren
 pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=LED_BRIGHTNESS, pixel_order=LED_ORDER, auto_write=False)
 
-# Read the airports file to retrieve list of airports and use as order for LEDs
-airports = [x.strip() for x in config["flugplaetze"]]
+# ICAO Codes der Flugplätze für die Reihenfolge als Liste
+flugplaetze = [x.strip() for x in config["flugplaetze"]]
 
+# METAR als XML abfragen
 url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=5&mostRecentForEachStation=true&stationString=" + ",".join(
-    [item for item in airports if item != "NULL"])
-
+    [item for item in flugplaetze if item != "NULL"])
 req = urllib.request.Request(url, headers={
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 Edg/86.0.622.69'})
 content = urllib.request.urlopen(req).read()
 
+# XML parsen
 root = ET.fromstring(content)
-conditionDict = {"NULL": {"flightCategory": "", "windDir": "", "windSpeed": 0, "windGustSpeed": 0, "windGust": False,
-                          "lightning": False, "tempC": 0, "dewpointC": 0, "vis": 0, "altimHg": 0, "obs": "",
-                          "skyConditions": {}, "obsTime": datetime.datetime.now()}}
-conditionDict.pop("NULL")
-stationList = []
 
+# dictionary für jede Beobachtung
+beobachtung = {}
+
+# über jedes METAR iterieren
 for metar in root.iter('METAR'):
-    stationId = metar.find('station_id').text
-    if metar.find('flight_category') is None:
-        print("Missing flight condition, skipping.")
-        continue
-    flightCategory = metar.find('flight_category').text
-    windDir = ""
-    windSpeed = 0
-    windGustSpeed = 0
-    windGust = False
-    lightning = False
-    tempC = 0
-    dewpointC = 0
-    vis = 0
-    altimHg = 0.0
-    obs = ""
-    skyConditions = []
-    if metar.find('wind_gust_kt') is not None:
-        windGustSpeed = int(metar.find('wind_gust_kt').text)
-        windGust = (True if (windGustSpeed > WIND_BLINK_THRESHOLD) else False)
-    if metar.find('wind_speed_kt') is not None:
-        windSpeed = int(metar.find('wind_speed_kt').text)
-    if metar.find('wind_dir_degrees') is not None:
-        windDir = metar.find('wind_dir_degrees').text
-    if metar.find('temp_c') is not None:
-        tempC = int(round(float(metar.find('temp_c').text)))
-    if metar.find('dewpoint_c') is not None:
-        dewpointC = int(round(float(metar.find('dewpoint_c').text)))
-    if metar.find('visibility_statute_mi') is not None:
-        vis = float(metar.find('visibility_statute_mi').text)
-        vis = round(vis / 0.62137119223733, 1)
-    if metar.find('altim_in_hg') is not None:
-        altimHg = float(round(float(metar.find('altim_in_hg').text), 2))
-    if metar.find('wx_string') is not None:
-        obs = metar.find('wx_string').text
-    if metar.find('observation_time') is not None:
-        obsTime = datetime.datetime.fromisoformat(metar.find('observation_time').text.replace("Z", "+00:00"))
-    for skyIter in metar.iter("sky_condition"):
-        skyCond = {"cover": skyIter.get("sky_cover"), "cloudBaseFt": int(skyIter.get("cloud_base_ft_agl", default=0))}
-        skyConditions.append(skyCond)
-    if metar.find('raw_text') is not None:
-        rawText = metar.find('raw_text').text
-        lightning = False if ((rawText.find('LTG', 4) == -1 and rawText.find('TS', 4) == -1) or rawText.find('TSNO', 4) != -1) else True
-    conditionDict[stationId] = {"flightCategory": flightCategory, "windDir": windDir, "windSpeed": windSpeed,
-                                "windGustSpeed": windGustSpeed, "windGust": windGust, "vis": vis, "obs": obs,
-                                "tempC": tempC, "dewpointC": dewpointC, "altimHg": altimHg, "lightning": lightning,
-                                "skyConditions": skyConditions, "obsTime": obsTime}
-    stationList.append(stationId)
+    # ICAO Code des Flugplatzes
+    stations_id = metar.find('station_id').text
 
+    # wenn keine flight_category vorhanden ist, dann überspringe das METAR
+    if metar.find('flight_category') is None:
+        logging.warning("Keine flight_category für " + stations_id)
+        continue
+
+    # Variablen für die Beobachtung
+    flightCategory = metar.find('flight_category').text
+    wind_richtung = ""
+    wind_geschwindigkeit_kt = 0
+    hoher_wind_knoten = 0
+    hoher_wind = False
+    blitze = False
+    temp_c = 0
+    taupunkt_c = 0
+    sichtweite_km = 0
+    luftdruck_inhg = 0.0
+    wettererscheinung = ""
+
+    # Liste für alle Wolkenuntergrenzen und Bedeckungsgrad für das jeweilige METAR
+    alle_wolken_bedingungen = []
+
+    # wenn es böig ist, dann setze hoher_wind auf True
+    if metar.find('wind_gust_kt') is not None:
+        hoher_wind_knoten = int(metar.find('wind_gust_kt').text)
+        hoher_wind = (True if (hoher_wind_knoten > WIND_BLINK_THRESHOLD) else False)
+
+    # Windgeschwindigkeit in knoten
+    if metar.find('wind_speed_kt') is not None:
+        wind_geschwindigkeit_kt = int(metar.find('wind_speed_kt').text)
+
+    # Windrichtung
+    if metar.find('wind_dir_degrees') is not None:
+        wind_richtung = metar.find('wind_dir_degrees').text
+
+    # Temperatur in °C
+    if metar.find('temp_c') is not None:
+        temp_c = int(round(float(metar.find('temp_c').text)))
+
+    # Taupunkt in °C
+    if metar.find('dewpoint_c') is not None:
+        taupunkt_c = int(round(float(metar.find('dewpoint_c').text)))
+
+    # Sichtweite in Statute Miles → Umrechnung in km
+    if metar.find('visibility_statute_mi') is not None:
+        sichtweite_sm = float(metar.find('visibility_statute_mi').text)
+        sichtweite_km = round(sichtweite_sm / 0.62137119223733, 1)
+
+    # Luftdruck in inHg
+    if metar.find('altim_in_hg') is not None:
+        luftdruck_inhg = float(round(float(metar.find('altim_in_hg').text), 2))
+
+    # weiter Wettererscheinungen (z.B. -RA)
+    if metar.find('wx_string') is not None:
+        wettererscheinung = metar.find('wx_string').text
+
+    # Zeitpunkt der Beobachtung
+    if metar.find('observation_time') is not None:
+        zeitpunkt = datetime.datetime.fromisoformat(metar.find('observation_time').text.replace("Z", "+00:00"))
+
+    # alle Wolkendecken mit Wolkenuntergrenze und Bedeckungsgrad erfassen
+    for wolken_iter in metar.iter("sky_condition"):
+        wolken_bedingung = {"cover": wolken_iter.get("sky_cover"), "cloudBaseFt": int(wolken_iter.get("cloud_base_ft_agl", default=0))}
+        alle_wolken_bedingungen.append(wolken_bedingung)
+
+    # METAR nach Wettererscheinungen die auf Blitze hindeuten filtern
+    if metar.find('raw_text') is not None:
+        metar_text = metar.find('raw_text').text
+        blitze = False if ((metar_text.find('LTG', 4) == -1 and metar_text.find('TS', 4) == -1) or metar_text.find('TSNO', 4) != -1) else True
+
+    # dictionary mit den Beobachtungen füllen
+    beobachtung[stations_id] = {"flightCategory": flightCategory, "windDir": wind_richtung, "windSpeed": wind_geschwindigkeit_kt,
+                                "windGustSpeed": hoher_wind_knoten, "windGust": hoher_wind, "vis": sichtweite_km, "obs": wettererscheinung,
+                                "tempC": temp_c, "dewpointC": taupunkt_c, "altimHg": luftdruck_inhg, "lightning": blitze,
+                                "skyConditions": alle_wolken_bedingungen, "obsTime": zeitpunkt}
+
+# Blinkfrequenz der LEDs berechnen
 looplimit = int(round(BLINK_TOTALTIME_SECONDS / BLINK_SPEED))
 
+# jede zweite Iteration wird bei blinkenden Lichtern die Farbe gewechselt
 windCycle = False
 
+# bis die Daten neu abgefragt werden (default alle 5 min)
 while looplimit > 0:
     i = 0
-    for airportcode in airports:
-        if airportcode == "NULL":
+    for icao in flugplaetze:
+        if icao == "NULL":
             i += 1
             continue
 
         color = COLOR_CLEAR
-        conditions = conditionDict.get(airportcode, None)
+        conditions = beobachtung.get(icao, None)
         windy = False
         highWinds = False
         lightningConditions = False
@@ -154,19 +187,19 @@ while looplimit > 0:
             if version == "gafor":
                 if conditions["vis"] < 1.5 or any(sc["cloudBaseFt"] < 500 and sc["cover"] in ("BKN", "OVC") for sc in conditions["skyConditions"]):
                     color = COLOR_GAFOR_X if not (windy or lightningConditions) else COLOR_GAFOR_BLITZE if lightningConditions else COLOR_GAFOR_X_WIND if windy else COLOR_CLEAR
-                    logging.debug(f"setting {airportcode} to X {conditions['vis']} {conditions['skyConditions']}")
+                    logging.debug(f"setting {icao} to X {conditions['vis']} {conditions['skyConditions']}; wind: {conditions['windSpeed']}")
                 elif (5 > conditions["vis"] >= 1.5) or any(1000 > sc["cloudBaseFt"] >= 500 and sc["cover"] in ("BKN", "OVC") for sc in conditions["skyConditions"]):
                     color = COLOR_GAFOR_M if not (windy or lightningConditions) else COLOR_GAFOR_BLITZE if lightningConditions else COLOR_GAFOR_M_WIND if windy else COLOR_CLEAR
-                    logging.debug(f"setting {airportcode} to M {conditions['vis']} {conditions['skyConditions']}")
+                    logging.debug(f"setting {icao} to M {conditions['vis']} {conditions['skyConditions']}; wind: {conditions['windSpeed']}")
                 elif (8 > conditions["vis"] >= 5) or any(2000 > sc["cloudBaseFt"] >= 1000 and sc["cover"] in ("BKN", "OVC") for sc in conditions["skyConditions"]):
                     color = COLOR_GAFOR_D if not (windy or lightningConditions) else COLOR_GAFOR_BLITZE if lightningConditions else COLOR_GAFOR_D_WIND if windy else COLOR_CLEAR
-                    logging.debug(f"setting {airportcode} to D {conditions['vis']} {conditions['skyConditions']}")
+                    logging.debug(f"setting {icao} to D {conditions['vis']} {conditions['skyConditions']}; wind: {conditions['windSpeed']}")
                 elif (10 > conditions["vis"] >= 8) or any(5000 > sc["cloudBaseFt"] >= 2000 and sc["cover"] in ("BKN", "OVC") for sc in conditions["skyConditions"]):
                     color = COLOR_GAFOR_O if not (windy or lightningConditions) else COLOR_GAFOR_BLITZE if lightningConditions else COLOR_GAFOR_O_WIND if windy else COLOR_CLEAR
-                    logging.debug(f"setting {airportcode} to O {conditions['vis']} {conditions['skyConditions']}")
+                    logging.debug(f"setting {icao} to O {conditions['vis']} {conditions['skyConditions']}; wind: {conditions['windSpeed']}")
                 elif (conditions["vis"] >= 10) or any(sc["cloudBaseFt"] >= 5000 and sc["cover"] in ("BKN", "OVC") for sc in conditions["skyConditions"]):
                     color = COLOR_GAFOR_C if not (windy or lightningConditions) else COLOR_GAFOR_BLITZE if lightningConditions else COLOR_GAFOR_C_WIND if windy else COLOR_CLEAR
-                    logging.debug(f"setting {airportcode} to C {conditions['vis']} {conditions['skyConditions']}")
+                    logging.debug(f"setting {icao} to C {conditions['vis']} {conditions['skyConditions']}; wind: {conditions['windSpeed']}")
                 else:
                     color = COLOR_CLEAR
 
