2919bc331a92eaccd0bc03659bf6e1e96631d9a2
ukena <48249968+ukena@users.noreply.github.com>
2022-12-17 16:35:44 +0100

    testing update behavior

--- karte/settings.py
+++ karte/settings.py
@@ -2,17 +2,18 @@ from gpiozero import Button
 import subprocess
 from signal import pause
 from git import Repo
+from time import sleep
 
-# TEST
 # Button instanziieren
 button = Button(23, hold_time=15)
 
 # Funktion wird ausgeführt, wenn der Button 3 Sekunden gedrückt wird
 def load_settings():
-    # LEDs ausschalten
-    subprocess.call(["sudo", "/home/metar/karte/lightsoff.sh"])
     # normales WLAN deaktivieren
     subprocess.call(["wpa_cli", "-i", "wlan0", "disable_network", "0"])
+    # LEDs ausschalten
+    subprocess.call(["sudo", "/home/metar/karte/lightsoff.sh"])
+    sleep(1)
     # normales WLAN aktivieren (Verbindung aber erst nach dem Laden der Einstellungen)
     subprocess.call(["wpa_cli", "-i", "wlan0", "enable_network", "0"])
 
